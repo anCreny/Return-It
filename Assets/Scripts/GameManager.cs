fd46058c72fb8c49cb898aaecdec7603b8dd8f8f
Creny <artem.0110@mail.ru>
2023-02-26 23:50:56 +0300

    Fixing animation and getting random spawnpoint at the begin of the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Burst;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviour
 {
-    private Vector2 spawnPoint = new (0, 3);
+    private Vector2 _spawnPoint = new (0, 3);
     [FormerlySerializedAs("shadow")] [SerializeField]
     private Animator shadowAnimator;
     [FormerlySerializedAs("ball")] [SerializeField]
@@ -16,26 +12,45 @@ public class GameManager : MonoBehaviour
     private GameObject ball;
     [SerializeField] 
     private GameObject shadow;
+
+    private bool _stupidFlag;
     void Start()
     {
+        _spawnPoint = GetRandomPoint();
+        shadow.transform.position = _spawnPoint;
         ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-        shadow.transform.position = spawnPoint;
         shadowAnimator.SetBool("StartShadow",true);
-        ballAnimator.SetBool("StartFall",true);
-        // ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
     }
 
     private void FixedUpdate()
     {
-        ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-        if ((shadowAnimator.GetCurrentAnimatorStateInfo(0).IsName("Shadow")))
-        {
-            ball.transform.position = spawnPoint;
-            // Destroy(shadow);
-        }
-        if ((ballAnimator.GetCurrentAnimatorStateInfo(0).IsName("Falling")))
+        if (_stupidFlag && !ballAnimator.GetCurrentAnimatorStateInfo(0).IsName("Falling"))
         {
             ball.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            ball.transform.localScale = new Vector3(1, 1, 1);
+            _stupidFlag = false;
         }
+
+        if (!ballAnimator.GetBool("StartFall") && !shadowAnimator.GetCurrentAnimatorStateInfo(0).IsName("Shadow"))
+        {
+            ball.transform.position = _spawnPoint;
+            ballAnimator.SetBool("StartFall",true);
+            shadow.transform.position = new Vector3(100, 100, 100);
+            _stupidFlag = true;
+        }
+    }
+
+    private Vector2 GetRandomPoint()
+    {
+        var width = Camera.main.pixelWidth;
+        var height = Camera.main.pixelHeight;
+
+        var randomScreenPoint = new Vector2(Random.Range(width * 0.1f, width * 0.9f), Random.Range(height * 0.6f, height * 0.9f));
+        return Camera.main.ScreenToWorldPoint(randomScreenPoint);
+    }
+
+    private void Update()
+    {
+        
     }
 }
