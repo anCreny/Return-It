2f0614f08ca418696fd63d38f2098ec3a3cb6444
anCreny <artem.0110@mail.ru>
2023-03-15 23:21:31 +0300

    docker-compose FIX

--- server_docker/server/UserContext.cs
+++ server_docker/server/UserContext.cs
@@ -11,7 +11,7 @@ public class UserContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         
-         optionsBuilder.UseMySql("server=localhost;user=root;port=8200;password=Terfar43$dG3ER#;database=return_it_data;",
+         optionsBuilder.UseMySql("server=return_it_db;user=root;port=5050;password=root;database=return_it_data;",
          new MySqlServerVersion(new Version(8, 0, 32)));
         
     
